d74c67cebf591d2ba2a915322e93d32875765ce1
saurongrajewo10 <saurongrajewo10@o2.pl>
2020-09-15 12:56:19 +0200

    Hangman 1.0

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -10,29 +10,42 @@ class Board extends React.Component {
   }
   secretWord = '';
   displayedWord = '';
+  errorCounter = 0;
 
   hiddenLetterReveal(letter) {
-for(let i=0; i<this.secretWord.length; i++){
-if(this.secretWord[i]===letter){
-this.displayedWord[i]=letter;
-}
-}
-}
+    let tempString = '';
+    let foundLetterCounter = 0;
+    for (let i = 0; i < this.secretWord.length; i++) {
+      if (this.secretWord[i] === letter) {
+        foundLetterCounter++;
+        tempString += letter;
+      }
+      if (this.displayedWord[i] !== '*') {
+        tempString += this.displayedWord[i];
+      }
+      if (this.secretWord[i] !== letter && this.displayedWord[i] === '*') {
+        tempString += '*';
+      }
+    }
+    console.log(foundLetterCounter)
+    if (foundLetterCounter === 0) {
+      this.errorCounter++;
+      console.log(this.errorCounter)
+    }
+    this.displayedWord = tempString;
+  }
 
   pickLetter(letter, index) {
     let tempState = this.state;
-    if (this.secretWord.includes(letter)) {
-      this.hiddenLetterReveal(letter)
-    }
+    this.hiddenLetterReveal(letter)
     tempState.alphabet.splice(index, 1);
     tempState.usedLetters.push(letter);
     this.setState(tempState);
   }
 
-
   render() {
-    this.secretWord = this.secretWord === '' ? randomWords() : this.secretWord;
-    this.displayedWord = this.secretWord.replace(/./g, '*');
+    this.secretWord = this.secretWord === '' ? randomWords().toUpperCase() : this.secretWord.toUpperCase();
+    this.displayedWord = this.displayedWord === '' ? this.secretWord.replace(/./g, '*') : this.displayedWord;
     return (
       <div>
         <div className="header">
@@ -46,7 +59,7 @@ this.displayedWord[i]=letter;
 
         <div className="main-content">
           <div className="game-image">
-            <img src={require('./../../assets/hangman0.png')} alt="test"></img>
+            <img src={require('./../../assets/hangman' + this.errorCounter + '.png')} alt="test"></img>
           </div>
           <div className="keyboard">
             <div className="header">
@@ -54,16 +67,20 @@ this.displayedWord[i]=letter;
 
             {
               this.state.alphabet.map((letter, index) => {
-                return <button className="button" key={letter} onClick={() => { this.pickLetter(letter, index) }}>{letter}</button>
+                return <button className="button" disabled={this.errorCounter >= 7} key={letter} onClick={() => { this.pickLetter(letter, index) }}>{letter}</button>
               })
             }
           </div>
-          <div>  <div className="header">Used letters <br /> </div>
-            {
-              this.state.usedLetters.map((letter) => {
-                return <div className="pickedLetter" key={letter}>{letter}</div>
-              })
-            }</div>
+          <div>
+            <div className="header">Used letters <br /></div>
+            <div className="used-letters"> 
+              {
+                this.state.usedLetters.map((letter) => {
+                  return <div className="picked-letter" key={letter}>{letter}</div>
+                })
+              }
+            </div>
+            </div>
         </div>
 
         <div className="footer">
