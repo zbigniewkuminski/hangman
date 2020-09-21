989da1b9e72ccd7fa645c459c1d96e16f8232e82
saurongrajewo10 <saurongrajewo10@o2.pl>
2020-09-21 19:00:29 +0200

    Landing page, new font and sounds add

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -7,11 +7,13 @@ class Board extends React.Component {
   state = {
     alphabet: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
     usedLetters: [],
+    welcomescreen: false,
   }
   secretWord = '';
   displayedWord = '';
   errorCounter = 0;
-  videoId='ZKAM_Hk4eZ0';
+  videoId = 'ZKAM_Hk4eZ0';
+  winVideoId='E-XoZAlEDkY';
 
   hiddenLetterReveal(letter) {
     let tempString = '';
@@ -28,18 +30,19 @@ class Board extends React.Component {
         tempString += '*';
       }
     }
-    if(tempString===this.secretWord){
-      this.errorCounter=8;
+    if (tempString === this.secretWord) {
+      this.errorCounter = 8;
+      this.videoId = this.winVideoId;
     }
     if (foundLetterCounter === 0) {
       this.errorCounter++;
     }
-    if(this.errorCounter===7){
-      this.displayedWord=this.secretWord;
-      this.videoId='RHYOZaQuqtM';
+    if (this.errorCounter === 7) {
+      this.displayedWord = this.secretWord;
+      this.videoId = 'RHYOZaQuqtM';
     }
-    else{
-    this.displayedWord = tempString;
+    else {
+      this.displayedWord = tempString;
     }
   }
 
@@ -61,48 +64,58 @@ class Board extends React.Component {
         autoplay: 1,
       },
     };
-    return (
-      <div>
-        <div className="header">
-          <h3> Your goal is to save the poor man from being hanged. You can achieve this by guessing all hidden letters.
-          Pick letters from board below but be aware that every mistake You make, gets this Guy closer to dead. Good luck.
+    if (this.state.welcomescreen) {
+      return (
+        <div>
+          <div className="header">
+            <h3> Your goal is to save the poor man from being hanged. You can achieve this by guessing all hidden letters.
+            Pick letters from board below but be aware that every mistake You make, gets this Guy closer to dead. Good luck.
           </h3>
-          <h1>
-            {this.displayedWord}
-          </h1>
-        </div>
-        <YouTube videoId={this.videoId} opts={opts} onReady={this._onReady} />
-        <div className="main-content">
-          <div className="game-image">
-            <img src={require('./../../assets/hangman' + this.errorCounter + '.png')} alt="test"></img>
+            <h1>
+              {this.displayedWord}
+            </h1>
           </div>
-          <div className="keyboard">
-            <div className="header">
-              Pick letter <br /> </div>
+          <YouTube videoId={this.videoId} opts={opts} onReady={this._onReady} />
+          <div className="main-content">
+            <div className="game-image">
+              <img src={require('./../../assets/hangman' + this.errorCounter + '.png')} alt="test"></img>
+            </div>
+            <div className="keyboard">
+              <div className="header">
+                Pick letter <br /> </div>
 
-            {
-              this.state.alphabet.map((letter, index) => {
-                return <button className="button" disabled={this.errorCounter >= 7} key={letter} onClick={() => { this.pickLetter(letter, index) }}>{letter}</button>
-              })
-            }
-          </div>
-          <div>
-            <div className="header">Used letters <br /></div>
-            <div className="used-letters">
               {
-                this.state.usedLetters.map((letter) => {
-                  return <div className="picked-letter" key={letter}>{letter}</div>
+                this.state.alphabet.map((letter, index) => {
+                  return <button className="button" disabled={this.errorCounter >= 7} key={letter} onClick={() => { this.pickLetter(letter, index) }}>{letter}</button>
                 })
               }
             </div>
+            <div>
+              <div className="header">Used letters <br /></div>
+              <div className="used-letters">
+                {
+                  this.state.usedLetters.map((letter) => {
+                    return <div className="picked-letter" key={letter}>{letter}</div>
+                  })
+                }
+              </div>
+            </div>
+          </div>
+
+          <div className="footer">
+
           </div>
         </div>
-
-        <div className="footer">
-
-        </div>
+      )
+    } else {
+      return (<div className='welcome-screen'>
+        <div>Enter the Hangman 2077</div>
+        <img src={require('./../../assets/hangmangif.gif')} className="hangman-gif"></img>
+        <button className="button-start"  onClick={() => { this.setState({welcomescreen: true,}) }}>Start</button>
       </div>
-    )
+      )
+
+    }
   }
 }
 
