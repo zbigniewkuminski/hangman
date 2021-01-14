eb0973c1723255839004b70ca6aa46a9c0f7dca3
Zbigniew Kuminski <zbigniew.kuminski@gmail.com>
2021-01-14 18:02:28 +0100

    new scoreboard design

--- src/components/AddPlayerToScoreboard/AddPlayerToScoreboard.js
+++ src/components/AddPlayerToScoreboard/AddPlayerToScoreboard.js
@@ -9,36 +9,36 @@ class AddPlayerToScoreboard extends React.Component {
 
 
   handleNewPlayerName = (event) => {
-    this.setState({newPlayerName: event.target.value})
+    this.setState({ newPlayerName: event.target.value })
   }
 
-  componentDidMount(){
-    this.getScoresFromLocalStorage();
+  componentDidMount() {
+    this.getScoresFromFireBase();
   }
 
-  async getScoresFromLocalStorage() {
-    const response = await fetch('https://hangman2077-55a3b-default-rtdb.europe-west1.firebasedatabase.app/.json');
+  async getScoresFromFireBase() {
+    const response = await fetch('https://hangman2077-55a3b-default-rtdb.europe-west1.firebasedatabase.app/scores/.json');
     const data = await response.json();
-    this.setState({topScores: data? data : []});
+    this.setState({ topScores: data ? data : [] });
   }
 
   closeScoreboard() {
     this.props.scoreboardDisplay();
   }
 
-  async prepareToSave(){
+  async prepareToSave() {
     var newScore = {
       name: this.state.newPlayerName,
       score: this.props.playerScore
     }
-    let updatedScoreboard = this.insertNewScore(this.state.topScores, newScore);
-    let requestOption = {method: 'PUT', body:JSON.stringify(updatedScoreboard), headers: { 'Content-Type': 'text/plain' },};
-    await fetch('https://hangman2077-55a3b-default-rtdb.europe-west1.firebasedatabase.app/.json', requestOption);
-    this.setState({topScores: updatedScoreboard});
+    let updatedScoreboard = this.updateScoresList(this.state.topScores, newScore);
+    let requestOption = { method: 'PUT', body: JSON.stringify(updatedScoreboard), headers: { 'Content-Type': 'text/plain' } };
+    await fetch('https://hangman2077-55a3b-default-rtdb.europe-west1.firebasedatabase.app/scores/.json', requestOption);
+    this.setState({ topScores: updatedScoreboard });
     this.props.gameReset(this.props.puzzleDiscovered);
   }
 
-  insertNewScore(unsortedScores, newScore) {
+  updateScoresList(unsortedScores, newScore) {
     var sortedScores = [];
     if (!unsortedScores.length) {
       sortedScores.push(newScore);
@@ -52,8 +52,8 @@ class AddPlayerToScoreboard extends React.Component {
       return unsortedScores;
     }
     for (var i = 0; i < unsortedScores.length - 1; i++) {
-      if (unsortedScores[i] && unsortedScores[i].score >=newScore.score &&
-         unsortedScores[i + 1].score < newScore.score) {
+      if (unsortedScores[i] && unsortedScores[i].score >= newScore.score &&
+        unsortedScores[i + 1].score < newScore.score) {
         sortedScores = unsortedScores.slice(0, i + 1);
         sortedScores.push(newScore);
         sortedScores = sortedScores.concat(unsortedScores.slice(i + 1, unsortedScores.length));
@@ -63,7 +63,7 @@ class AddPlayerToScoreboard extends React.Component {
     return sortedScores.slice(0, 10);
   }
 
-  checkIsNewScoreInTop10(){
+  checkIsNewScoreInTop10() {
     return this.state.topScores.length < 10 || this.state.topScores[9].score < this.props.playerScore ? true : false;
   }
 
@@ -83,30 +83,80 @@ class AddPlayerToScoreboard extends React.Component {
               X
             </button>
           </h1>
+          <div className="podium">
+            <div className="first-place">
+              {(() => {
+                return (
+                  <div>
+                    <div >1.</div>
+                    <div >
+                      {this.state.topScores[0]
+                        ? this.state.topScores[0].name : "........"}
+                      {this.state.topScores[0]
+                        ? ' ' + this.state.topScores[0].score
+                        : "..."}
+                    </div>
+                  </div>
+                )
+              })()}
+            </div>
+            <div className="second-place">
+              {(() => {
+                return (
+                  <div>
+                    <div >2.</div>
+                    <div >
+                      {this.state.topScores[1]
+                        ? this.state.topScores[1].name : "........"}
+                      {this.state.topScores[1]
+                        ? ' ' + this.state.topScores[1].score
+                        : "..."}
+                    </div>
+                  </div>
+                )
+              })()}
+            </div>
+            <div className="third-place">
+              {(() => {
+                return (
+                  <div>
+                    <div >3.</div>
+                    <div >
+                      {this.state.topScores[2]
+                        ? this.state.topScores[2].name : "........"}
+                      {this.state.topScores[2]
+                        ? ' ' + this.state.topScores[2].score
+                        : "..."}
+                    </div>
+                  </div>
+                )
+              })()}
+            </div>
+          </div>
           <div className="scores-displaying">
             <div className="scores-displaying-single-element">
-              {[...Array(10)].map((x, i) => {
-                return <div key={i}>{i + 1}. </div>;
+              {[...Array(7)].map((x, i) => {
+                return <div key={i + 3}>{i + 4}. </div>;
               })}
             </div>
             <div className="scores-displaying-single-element">
-              {[...Array(10)].map((x, i) => {
+              {[...Array(7)].map((x, i) => {
                 return (
-                  <div key={i}>
+                  <div key={i + 3}>
                     {" "}
-                    {this.state.topScores[i]
-                      ? this.state.topScores[i].name : "........"}
+                    {this.state.topScores[i + 3]
+                      ? this.state.topScores[i + 3].name : "........"}
                   </div>
                 );
               })}
             </div>
             <div className="scores-displaying-single-element">
-              {[...Array(10)].map((x, i) => {
+              {[...Array(7)].map((x, i) => {
                 return (
-                  <div key={i}>
+                  <div key={i + 3}>
                     {" "}
-                    {this.state.topScores[i]
-                      ? this.state.topScores[i].score
+                    {this.state.topScores[i + 3]
+                      ? this.state.topScores[i + 3].score
                       : "..."}
                   </div>
                 );
@@ -125,8 +175,8 @@ class AddPlayerToScoreboard extends React.Component {
                 </div>
               )
             }
-            if(this.props.showNameInput && !this.checkIsNewScoreInTop10()){
-              return('You are weak and you do not deserve for a place in scoreboard.')
+            if (this.props.showNameInput && !this.checkIsNewScoreInTop10()) {
+              return ('You are weak and you do not deserve for a place in scoreboard.')
             }
           })()}
         </div>
