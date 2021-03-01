f23e46ad6484afe72b768c13cd2939a0deca1f1a
Zbigniew Kuminski <zbigniew.kuminski@gmail.com>
2021-03-01 18:03:16 +0100

    PR comments resolved

--- src/components/AddPlayerToScoreboard/AddPlayerToScoreboard.js
+++ src/components/AddPlayerToScoreboard/AddPlayerToScoreboard.js
@@ -14,7 +14,16 @@ class AddPlayerToScoreboard extends React.Component {
 
   componentDidMount() {
     this.getScoresFromFireBase();
+    window.addEventListener('resize', this.updateDimensions);
   }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
+  }
+
+  updateDimensions = () => {
+    this.forceUpdate();
+  };
 
   async getScoresFromFireBase() {
     const response = await fetch('https://hangman2077-55a3b-default-rtdb.europe-west1.firebasedatabase.app/scores/.json');
@@ -71,13 +80,14 @@ class AddPlayerToScoreboard extends React.Component {
     return (
       <div className="component-background">
         <div className="add-player-to-scoreboard">
-          <div className="row justify-content-center align-items-center">
-            <div>
-              <h2 className="mt-2">
+          <div className="row justify-content-between align-items-center">
+            <div className="col-md-1 col-sm-2 col-2"></div>
+            <div className="col-md-10 col-sm-8 col-8">
+              <h2 className="mt-2 add-player-to-scoreboard-title">
                 {this.props.languageVersion?.scoreboardDescription.toUpperCase()}
               </h2>
             </div>
-            <div>
+            <div className="col-md-1 col-sm-2 col-2">
               <button
                 className="button pull-right"
                 onClick={() => {
@@ -86,119 +96,145 @@ class AddPlayerToScoreboard extends React.Component {
               >X</button>
             </div>
           </div>
-          <div className="row">
-            <div className="col-md-4 d-flex align-items-end">
-              {(() => {
-                return (
-                  <div className="second-place">
-                    <div >2.</div>
-                    <div >
-                      {this.state.topScores[1]
-                        ? this.state.topScores[1].name : "........"}
-                      {this.state.topScores[1]
-                        ? ' ' + this.state.topScores[1].score
-                        : "..."}
-                    </div>
-                    <div className="podium-second">
-                    </div>
-                  </div>
-                )
-              })()}
-            </div>
-            <div className="col-md-4 d-flex align-items-end">
-              {(() => {
-                return (
-                  <div className="first-place">
-                    <div >1.</div>
-                    <div >
-                      {this.state.topScores[0]
-                        ? this.state.topScores[0].name : "........"}
-                      {this.state.topScores[0]
-                        ? ' ' + this.state.topScores[0].score
-                        : "..."}
-                    </div>
-                    <div className="podium-first">
-                    </div>
-                  </div>
-                )
-              })()}
-            </div>
-            <div className="col-md-4 d-flex align-items-end">
-              {(() => {
-                return (
-                  <div className="third-place">
-                    <div >3.</div>
-                    <div >
-                      {this.state.topScores[2]
-                        ? this.state.topScores[2].name : "........"}
-                      {this.state.topScores[2]
-                        ? ' ' + this.state.topScores[2].score
-                        : "..."}
-                    </div>
-                    <div className="podium-third">
-                    </div>
-                  </div>
-                )
-              })()}
-            </div>
-          </div>
-          <div className="scores-displaying">
-            <div className="row justify-content-center">
-              <div className="scores-displaying-single-element">
-                {[...Array(7)].map((x, i) => {
-                  return <div key={i + 3}>{i + 4}. </div>;
-                })}
-              </div>
-              <div className="scores-displaying-single-element">
-                {[...Array(7)].map((x, i) => {
-                  return (
-                    <div key={i + 3}>
-                      {" "}
-                      {this.state.topScores[i + 3]
-                        ? this.state.topScores[i + 3].name : "........"}
-                    </div>
-                  );
-                })}
-              </div>
-              <div className="scores-displaying-single-element">
-                {[...Array(7)].map((x, i) => {
-                  return (
-                    <div key={i + 3}>
-                      {" "}
-                      {this.state.topScores[i + 3]
-                        ? this.state.topScores[i + 3].score
-                        : "..."}
-                    </div>
-                  );
-                })}
-              </div>
-            </div>
-          </div>
           {(() => {
-            if (this.props.showNameInput && this.checkIsNewScoreInTop10()) {
+            if (window.innerWidth > 783) {
               return (
-                <div className="mt-3">
-                  <div className="row justify-content-center">
-                    <input className="name-input" value={this.state.newPlayerName} maxLength='20' type='text' onChange={this.handleNewPlayerName} placeholder={this.props.languageVersion.typeNameDescription} />
-                    <div className="score">{this.props.languageVersion.scoredDescription} {this.props.playerScore} {this.props.languageVersion.ptsDescription}</div>
-                  </div>
-                  <div className="row justify-content-center">
-                    <button className="button-save" disabled={!this.state.newPlayerName.trim()} onClick={() => { this.prepareToSave() }}>{this.props.languageVersion.saveDescription}</button>
-                  </div>
+                <div>
+                  {this.renderPodium()}
+                  {this.renderScoresList(7, 3)}
+                </div>
+              )
+            } else {
+              return (
+                <div>
+                  { this.renderScoresList(10, 0)}
                 </div>
               )
             }
-            if (this.props.showNameInput && !this.checkIsNewScoreInTop10()) {
-              return (<div className="row">
-                <div className="col-md-12 low-score-info">
-                  {this.props.languageVersion.toWeakDescription}
-                </div>
-              </div>)
-            }
           })()}
         </div>
-      </div >
+        {(() => {
+          if (this.props.showNameInput && this.checkIsNewScoreInTop10()) {
+            return (
+              <div className="mt-3">
+                <div className="row justify-content-center">
+                  <input className="name-input" value={this.state.newPlayerName} maxLength='20' type='text' onChange={this.handleNewPlayerName} placeholder={this.props.languageVersion.typeNameDescription} />
+                  <div className="score">{this.props.languageVersion.scoredDescription} {this.props.playerScore} {this.props.languageVersion.ptsDescription}</div>
+                </div>
+                <div className="row justify-content-center">
+                  <button className="button-save" disabled={!this.state.newPlayerName.trim()} onClick={() => { this.prepareToSave() }}>{this.props.languageVersion.saveDescription}</button>
+                </div>
+              </div>
+            )
+          }
+          if (this.props.showNameInput && !this.checkIsNewScoreInTop10()) {
+            return (<div className="row">
+              <div className="col-md-12 low-score-info">
+                {this.props.languageVersion.toWeakDescription}
+              </div>
+            </div>)
+          }
+        })()}
+      </div>
     );
+  }
+
+  renderPodium() {
+    return (
+      <div className="row">
+        <div className="col-md-4 d-flex align-items-end">
+          {(() => {
+            return (
+              <div className="second-place">
+                <div >2.</div>
+                <div >
+                  {this.state.topScores[1]
+                    ? this.state.topScores[1].name : "........"}
+                  {this.state.topScores[1]
+                    ? ' ' + this.state.topScores[1].score
+                    : "..."}
+                </div>
+                <div className="podium-second">
+                </div>
+              </div>
+            )
+          })()}
+        </div>
+        <div className="col-md-4 d-flex align-items-end">
+          {(() => {
+            return (
+              <div className="first-place">
+                <div >1.</div>
+                <div >
+                  {this.state.topScores[0]
+                    ? this.state.topScores[0].name : "........"}
+                  {this.state.topScores[0]
+                    ? ' ' + this.state.topScores[0].score
+                    : "..."}
+                </div>
+                <div className="podium-first">
+                </div>
+              </div>
+            )
+          })()}
+        </div>
+        <div className="col-md-4 d-flex align-items-end">
+          {(() => {
+            return (
+              <div className="third-place">
+                <div >3.</div>
+                <div >
+                  {this.state.topScores[2]
+                    ? this.state.topScores[2].name : "........"}
+                  {this.state.topScores[2]
+                    ? ' ' + this.state.topScores[2].score
+                    : "..."}
+                </div>
+                <div className="podium-third">
+                </div>
+              </div>
+            )
+          })()}
+        </div>
+      </div>
+    )
+  }
+
+  renderScoresList(listLength, indexesSkipped) {
+    return (
+      <div className="scores-displaying">
+        <div className="row justify-content-center">
+          <div className="scores-displaying-single-element">
+            {[...Array(listLength)].map((x, i) => {
+              return <div key={i + indexesSkipped}>{i + indexesSkipped + 1}. </div>;
+            })}
+          </div>
+          <div className="scores-displaying-single-element">
+            {[...Array(listLength)].map((x, i) => {
+              return (
+                <div key={i + indexesSkipped}>
+                  {" "}
+                  {this.state.topScores[i + indexesSkipped]
+                    ? this.state.topScores[i + indexesSkipped].name : "........"}
+                </div>
+              );
+            })}
+          </div>
+          <div className="scores-displaying-single-element">
+            {[...Array(listLength)].map((x, i) => {
+              return (
+                <div key={i + indexesSkipped}>
+                  {" "}
+                  {this.state.topScores[i + indexesSkipped]
+                    ? this.state.topScores[i + indexesSkipped].score
+                    : "..."}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
