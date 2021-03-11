8a2a273e15dcd4519cf97ed0ccc64e0c8b100f0f
Zbigniew Kuminski <zbigniew.kuminski@gmail.com>
2021-03-11 18:34:32 +0100

    Updates

---
+++ precache-manifest.ab304b44ae1f5d22e9837078121ffeeb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3abfd3e8bd41e503a9f1dd94752910",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "66c972d42ef5965f833f",
+    "url": "/hangman/static/css/2.8d3f7698.chunk.css"
+  },
+  {
+    "revision": "dc3a478b5ef20fa40ddc",
+    "url": "/hangman/static/css/main.dde5cb2c.chunk.css"
+  },
+  {
+    "revision": "66c972d42ef5965f833f",
+    "url": "/hangman/static/js/2.8e1af589.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/hangman/static/js/2.8e1af589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc3a478b5ef20fa40ddc",
+    "url": "/hangman/static/js/main.8be2e806.chunk.js"
+  },
+  {
+    "revision": "7ef95293683d0ed87720",
+    "url": "/hangman/static/js/runtime-main.6b115b9b.js"
+  },
+  {
+    "revision": "2c404fd06cd67770807d242b2d2e5a16",
+    "url": "/hangman/static/media/PressStart2P-Regular.2c404fd0.ttf"
+  },
+  {
+    "revision": "25dd885a1bc56147e4a6ee1f8e025d2b",
+    "url": "/hangman/static/media/background.25dd885a.png"
+  },
+  {
+    "revision": "b0946955c47beca561acecc50194ecbc",
+    "url": "/hangman/static/media/background3.b0946955.png"
+  },
+  {
+    "revision": "6532c7c3bac6beeef8ed9090b43bc8f4",
+    "url": "/hangman/static/media/background4.6532c7c3.png"
+  },
+  {
+    "revision": "b72d8e65b26ce131768a695e768c2ec3",
+    "url": "/hangman/static/media/hangman1.b72d8e65.png"
+  },
+  {
+    "revision": "ff64fa744b505f6fc0ff9a81f34b9ca6",
+    "url": "/hangman/static/media/hangman10.ff64fa74.png"
+  },
+  {
+    "revision": "a957ee08ed1d6dd1497791369b477c80",
+    "url": "/hangman/static/media/hangman3.a957ee08.png"
+  },
+  {
+    "revision": "be432228fce8daeff98ae4a19ad8fad6",
+    "url": "/hangman/static/media/hangman4.be432228.png"
+  },
+  {
+    "revision": "816c246a13875591c355b7d2602fb052",
+    "url": "/hangman/static/media/hangman5.816c246a.png"
+  },
+  {
+    "revision": "394f3d9825ad2ebf7c58575c892a806b",
+    "url": "/hangman/static/media/hangman7.394f3d98.png"
+  },
+  {
+    "revision": "402a0e12dc328ce34973fe1522a78cb7",
+    "url": "/hangman/static/media/hangman9.402a0e12.png"
+  },
+  {
+    "revision": "174b8704cbe61d9197bc2cbdcc179831",
+    "url": "/hangman/static/media/hangman_home_page.174b8704.gif"
+  }
+]);
